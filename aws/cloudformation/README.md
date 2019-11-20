81de2b40615e2fcfe94b5f044f9ba49f0b6196bf
Israel Fruchter <fruch@scylladb.com>
2019-11-20 23:21:33 +0300

    Added CloudFormation template

---
+++ aws/cloudformation/README.md
@@ -0,0 +1,36 @@
+## Installation
+```
+python3 -m venv .venv3
+source .venv3/bin/activate
+install -r requirements.txt
+```
+
+## Testing 
+``` bash
+# regenerate the teample
+jinja2 scylla.yaml.j2  > scylla.yaml
+
+# lint the template
+cfn-lint --template scylla.yaml --region us-east-1
+
+# running the test
+pytest --log-cli-level info
+```
+
+# Using the template
+```
+# run the template from command line
+aws cloudformation create-stack --region eu-west-1 --stack-name fruch-test-05 --template-body file://scylla.yaml \
+    --parameters  ParameterKey=KeyName,ParameterValue=scylla-qa-ec2 \
+    ParameterKey=InstanceType,ParameterValue=i3.large \
+    ParameterKey=AvailabilityZone,ParameterValue=eu-west-1a \
+    ParameterKey=ClusterName,ParameterValue=fruch \
+    ParameterKey=InstanceCount,ParameterValue=3 \
+    ParameterKey=ScyllaAmi,ParameterValue=ami-0ececa5cacea302a8
+
+```
+Example of link to start the cloudforamtion:
+
+https://eu-west-1.console.aws.amazon.com/cloudformation/home?region=eu-west-1#/stacks/create/review?templateURL=https://s3-eu-west-1.amazonaws.com/cf-templates-1jy8um4tbzwit-eu-west-1/2019241R3e-scylla.templateenk889k0zz&stackName=fruch-test&param_ScyllaAmi=ami-0ececa5cacea302a8
+   
+
---
+++ aws/cloudformation/test_scylla_cfn.py
@@ -0,0 +1,137 @@
+import pprint
+import logging
+import subprocess
+import uuid
+
+import pytest
+import boto3
+from botocore.errorfactory import ClientError
+
+
+def run_on_node(node_ip, cmd):
+    output = subprocess.run(
+        [
+            "/bin/bash",
+            "-c",
+            f'ssh -i ~/.ssh/scylla-qa-ec2  -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" centos@{node_ip} {cmd}',
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=False,
+    )
+    logging.info(output.stderr.decode("utf-8").strip())
+    return output.stdout.decode("utf-8").strip()
+
+
+@pytest.fixture(scope="session")
+def cfn_scylla_cluster(request):
+    region = request.config.getoption("--region")
+    client = boto3.client("cloudformation", region_name=region)
+    if request.config.getoption("--stack-name"):
+        name = request.config.getoption("--stack-name")
+    else:
+        rand_suffix = uuid.uuid4().hex[:6]
+        name = f"scylla-cfn-test-{rand_suffix}"
+
+    try:
+        response = client.create_stack(
+            StackName=name,
+            TemplateBody=open("scylla.yaml").read(),
+            Parameters=[
+                {"ParameterKey": "KeyName", "ParameterValue": "scylla-qa-ec2"},
+                {"ParameterKey": "InstanceType", "ParameterValue": "i3.large"},
+                {"ParameterKey": "AvailabilityZone", "ParameterValue": "us-east-1a"},
+                {"ParameterKey": "ClusterName", "ParameterValue": name},
+                {"ParameterKey": "InstanceCount", "ParameterValue": "3"},
+                {
+                    "ParameterKey": "ScyllaAmi",
+                    "ParameterValue": request.config.getoption("--ami"),
+                },
+            ],
+        )
+        logging.info(pprint.pformat(response))
+
+    except ClientError as ex:
+        logging.info(ex)
+
+    logging.info(f"waiting for cloudformation [{name}] to complete")
+
+    waiter = client.get_waiter("stack_create_complete")
+    response = waiter.wait(StackName=name)
+
+    resources = client.list_stack_resources(StackName=name)
+
+    outputs = client.describe_stacks(StackName=name)
+    logging.info(pprint.pformat(outputs))
+    outputs = {
+        item["OutputKey"]: item["OutputValue"]
+        for item in outputs["Stacks"][0]["Outputs"]
+    }
+
+    yield resources, outputs
+
+    if not request.session.testsfailed and not request.config.getoption("--keep-cfn"):
+        response = client.delete_stack(StackName=name)
+        logging.info(response)
+
+        waiter = client.get_waiter("stack_delete_complete")
+        response = waiter.wait(StackName=name)
+        logging.info(response)
+
+
+def wait_nodes_ready(resources, region):
+    ec2 = boto3.client("ec2", region_name=region)
+    ok_waiter = ec2.get_waiter("instance_status_ok")
+
+    instances = [
+        r["PhysicalResourceId"]
+        for r in resources["StackResourceSummaries"]
+        if r["ResourceType"] == "AWS::EC2::Instance"
+    ]
+
+    logging.debug(pprint.pformat(instances))
+
+    response = ok_waiter.wait(InstanceIds=instances)
+    logging.info(pprint.pformat(response))
+
+
+def test_cluster_up(request, cfn_scylla_cluster):
+    resources, outputs = cfn_scylla_cluster
+    region = request.config.getoption("--region")
+    wait_nodes_ready(resources, region)
+
+
+def test_connect(cfn_scylla_cluster):
+    resources, outputs = cfn_scylla_cluster
+    nodes_ip_addresses = [v for k, v in outputs.items() if "PublicIp" in k]
+
+    for node_ip in nodes_ip_addresses:
+        logging.info(f"connecting to node {node_ip}")
+        output = run_on_node(node_ip, "scylla --version")
+        logging.info(output)
+
+
+def test_nodetool_status(cfn_scylla_cluster):
+    resources, outputs = cfn_scylla_cluster
+    nodes_ip_addresses = [v for k, v in outputs.items() if "PublicIp" in k]
+    node_private_ips = [v for k, v in outputs.items() if "PrivateIp" in k]
+
+    for node_ip in nodes_ip_addresses:
+        logging.info(f"running nodetool on node {node_ip}")
+        output = run_on_node(node_ip, "nodetool status")
+        logging.info(output)
+        for private_ip in node_private_ips:
+            assert f"UN  {private_ip}" in output
+
+
+def test_cassandra_stress(cfn_scylla_cluster):
+    resources, outputs = cfn_scylla_cluster
+    nodes_ip_addresses = [v for k, v in outputs.items() if "PublicIp" in k]
+
+    for node_ip in nodes_ip_addresses:
+        logging.info(f"running c-s to node {node_ip}")
+        output = run_on_node(
+            node_ip, "cassandra-stress write n=40000 -rate threads=40 -node 172.31.0.11"
+        )
+        logging.info(output)
+        assert "Total errors              :          0 [WRITE: 0]" in output
