1bd52cd33cdedae490c874a448fed57cec1dbd73
Tzach Livyatan <tzach@scylladb.com>
2020-05-21 23:45:14 +0300

    use sphinx_scylladb_theme

--- source/conf.py
+++ source/conf.py
@@ -27,10 +27,10 @@ author = 'ScyllaDB'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxjsondomain']
+extensions = ['sphinxjsondomain', 'sphinx_scylladb_theme']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -43,9 +43,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_scylladb_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
