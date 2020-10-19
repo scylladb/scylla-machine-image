2fd00b22ee4411beef061620009ede4198eaf0fb
Yaron Kaikov <yaronkaikov@scylladb.com>
2020-10-19 09:36:18 +0300

    build_image.sh: get RPM version without downloading packages
    Today before building gce image, we need to get from repo the relevant
    packages version, in order to do it we are actually downloading the
    package although we are not using it
    
    Setting the option to get package version information without
    downloading the packages

--- gce/image/build_image.sh
+++ gce/image/build_image.sh
@@ -65,11 +65,17 @@ while [ $# -gt 0 ]; do
     esac
 done
 
-get_version_from_rpm () {
+get_version_from_local_rpm () {
+  RPM=$1
+  RELEASE=$(rpm -qi $RPM | awk '/Release/ { print $3 }' )
+  VERSION=$(rpm -qi $RPM | awk '/Version/ { print $3 }' )
+  echo "$VERSION-$RELEASE"
+}
+
+get_version_from_remote_rpm () {
     RPM=$1
-    RELEASE=$(rpm -qi $RPM | awk '/Release/ { print $3 }' )
-    VERSION=$(rpm -qi $RPM | awk '/Version/ { print $3 }' )
-    echo "$VERSION-$RELEASE"
+    VERSION=$(yum provides $RPM | grep "Provide.*=" | awk '{print $5}')
+    echo "$VERSION"
 }
 
 check_rpm_exists () {
@@ -114,21 +120,15 @@ else
 
     TMPREPO=$(mktemp -u -p /etc/yum.repos.d/ --suffix .repo)
     sudo curl -o $TMPREPO $REPO_FOR_INSTALL
-    rm -rf build/scylla_packages
-    mkdir -p build/scylla_packages
-    cd build/scylla_packages/
-    yumdownloader $PRODUCT $PRODUCT-kernel-conf $PRODUCT-conf $PRODUCT-server $PRODUCT-debuginfo $PRODUCT-machine-image $PRODUCT-jmx $PRODUCT-tools-core $PRODUCT-tools $PRODUCT-python3
+
+    SCYLLA_VERSION=$(get_version_from_remote_rpm $PRODUCT-server)
+    SCYLLA_MACHINE_IMAGE_VERSION=$(get_version_from_remote_rpm $PRODUCT-machine-image)
+    SCYLLA_JMX_VERSION=$(get_version_from_remote_rpm $PRODUCT-jmx)
+    SCYLLA_TOOLS_VERSION=$(get_version_from_remote_rpm $PRODUCT-tools)
+    SCYLLA_PYTHON3_VERSION=$(get_version_from_remote_rpm $PRODUCT-python3)
+
     sudo rm -f $TMPREPO
 
-    check_rpm_exists $DIR/build/scylla_packages
-
-    SCYLLA_VERSION=$(get_version_from_rpm $DIR/build/scylla_packages/$PRODUCT-server*.x86_64.rpm)
-    SCYLLA_MACHINE_IMAGE_VERSION=$(get_version_from_rpm $DIR/build/scylla_packages/$PRODUCT-machine-image*.noarch.rpm)
-    SCYLLA_JMX_VERSION=$(get_version_from_rpm $DIR/build/scylla_packages/$PRODUCT-jmx*.noarch.rpm)
-    SCYLLA_TOOLS_VERSION=$(get_version_from_rpm $DIR/build/scylla_packages/$PRODUCT-tools-*.noarch.rpm)
-    SCYLLA_PYTHON3_VERSION=$(get_version_from_rpm $DIR/build/scylla_packages/$PRODUCT-python3*.x86_64.rpm)
-
-    cd -
 fi
 
 
