d02c13e8c1f87851aba29721dd21d2ada04755df
Yaron Kaikov <yaronkaikov@scylladb.com>
2021-02-22 20:09:18 +0200

    scylla_install_image:install based on prodcut
    When building GCE image in enterprise the build pass although we getting en error as mention below:
    7:31:42  [0;32m    googlecompute: Scylla for Centos 8 - x86_64                     34 kB/s |  19 kB     00:00[0m
    17:31:42  [0;32m    googlecompute: Scylla for centos 8                             8.8 kB/s | 3.8 kB     00:00[0m
    17:31:43  [0;32m    googlecompute: No match for argument: scylla-python3[0m
    17:31:43  [0;32m    googlecompute: No match for argument: scylla-machine-image[0m
    17:31:43  [0;32m    googlecompute: No match for argument: scylla-debuginfo[0m
    (referance https://jenkins.scylladb.com/job/enterprise-2021.1/job/gce-image/7/consoleFull)
    
    Let's start installing scylla in GCE image based on product , so it will
    work for both open source and enterprise
    
    (cherry picked from commit b2cebeaef519885a0b319620a12ed86124f8f016)

--- gce/image/build_image.sh
+++ gce/image/build_image.sh
@@ -59,6 +59,7 @@ while [ $# -gt 0 ]; do
             ;;
         "--product")
             PRODUCT=$2
+            INSTALL_ARGS="$INSTALL_ARGS --product $2"
             shift 2
             ;;
         "--build-id")
