56a6006722a7adc45998a118bceb85014185c7f9
Yaron Kaikov <yaronkaikov@scylladb.com>
2024-01-30 20:09:39 +0200

    images: clean old and un-used code
    since they not in use anymore removing them

--- aws/cloudformation/conftest.py
+++
@@ -1,5 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption("--keep-cfn", action="store_true", default=False)
-    parser.addoption("--stack-name", action="store", default=None)
-    parser.addoption("--region", action="store", default="us-east-1")
-    parser.addoption("--ami", action="store", default="ami-0791dd37ca6da66c5")
