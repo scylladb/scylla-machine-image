e7d9e530a0c1c82afca4503feb701b50c5614400
Israel Fruchter <fruch@scylladb.com>
2025-12-09 22:21:13 +0200

    Apply all `ruff check` and `ruff format`

--- lib/param_estimation.py
+++ lib/param_estimation.py
@@ -1,6 +1,7 @@
 import json
-import yaml
+
 from lib.scylla_cloud import get_cloud_instance, is_ec2
+
 
 def estimate_streaming_bandwidth():
     cloud_instance = get_cloud_instance()
@@ -9,12 +10,11 @@ def estimate_streaming_bandwidth():
         instance_type = cloud_instance.instancetype
         # Generated with
         # aws ec2 describe-instance-types --query "InstanceTypes[].[InstanceType, NetworkInfo.NetworkPerformance, NetworkInfo.NetworkCards[0].BaselineBandwidthInGbps]" --output json
-        with open('/opt/scylladb/scylla-machine-image/aws_net_params.json') as f:
+        with open("/opt/scylladb/scylla-machine-image/aws_net_params.json") as f:
             netinfo = json.load(f)
             instance_info = [info for info in netinfo if info[0] == instance_type]
             if len(instance_info) != 0:
-                net_bw = int(instance_info[0][2] * 1024 * 1024 * 1024) #gbps -> bps
+                net_bw = int(instance_info[0][2] * 1024 * 1024 * 1024)  # gbps -> bps
     # TODO: other clouds
 
-    return int((.75 * net_bw) / (8 * 1024*1024)) # MB/s
-
+    return int((0.75 * net_bw) / (8 * 1024 * 1024))  # MB/s
--- tests/test_scylla_post_start.py
+++ tests/test_scylla_post_start.py
@@ -2,20 +2,24 @@
 # Copyright 2020 ScyllaDB
 #
 # SPDX-License-Identifier: Apache-2.0
-import sys
-import logging
-from unittest import TestCase
-from pathlib import Path
-import tempfile
 import base64
 import json
+import logging
+import sys
+import tempfile
+from pathlib import Path
+from unittest import TestCase
+
 
 sys.path.append(str(Path(__file__).parent.parent))
 
-from lib.log import setup_logging
-from common.scylla_post_start import ScyllaMachineImagePostStart
-
+import pytest
 from test_scylla_configure import DummyCloudInstance, TestScyllaConfigurator
+
+from common.scylla_post_start import ScyllaMachineImagePostStart
+from lib.log import setup_logging
+
+
 LOGGER = logging.getLogger(__name__)
 
 
@@ -24,7 +28,6 @@ def b64(s):
 
 
 class TestScyllaPostStart(TestCase):
-
     def setUp(self):
         LOGGER.info("Setting up test dir")
         self.temp_dir = tempfile.TemporaryDirectory()
@@ -37,11 +40,11 @@ class TestScyllaPostStart(TestCase):
         self.temp_dir.cleanup()
 
     def run_scylla_post_start(self, user_data):
-        self.post_start._cloud_instance = DummyCloudInstance(user_data=user_data, private_ipv4='127.0.0.1')
+        self.post_start._cloud_instance = DummyCloudInstance(user_data=user_data, private_ipv4="127.0.0.1")
         self.post_start.run_post_start_script()
 
     def test_no_user_data(self):
-        self.run_scylla_post_start('')
+        self.run_scylla_post_start("")
 
     def test_empty_script(self):
         self.run_scylla_post_start('{"post_start_script": ""}')
@@ -50,12 +53,11 @@ class TestScyllaPostStart(TestCase):
         self.run_scylla_post_start(json.dumps({"post_start_script": b64("echo start")}))
 
     def test_base64_encoding_error(self):
-        with self.assertRaises(SystemExit):
-            self.run_scylla_post_start(
-                json.dumps({"post_start_script": b64("non-existing-command")}))
+        with pytest.raises(SystemExit):
+            self.run_scylla_post_start(json.dumps({"post_start_script": b64("non-existing-command")}))
 
     def test_mime_multipart(self):
         scylla_user_data = {"post_start_script": b64("echo start")}
-        for data_type in ('json', 'yaml'):
+        for data_type in ("json", "yaml"):
             msg = TestScyllaConfigurator.multipart_user_data(scylla_user_data, data_type)
             self.run_scylla_post_start(user_data=str(msg))
